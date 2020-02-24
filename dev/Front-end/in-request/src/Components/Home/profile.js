b4f6b7dc879170f54eb936ca66bb3dc08e9353ef
ShiwenXu <vicky.xu071@gmail.com>
2020-02-24 13:39:58 -0500

    inital view

---
+++ dev/Front-end/in-request/src/Components/Home/profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+/* --- THIS FILE DISPLAY USER'S PERSONAL INFO --- */
+class Profile extends Component {
+  render() {
+    return <span>PROFILE PAGE</span>;
+  }
+}
