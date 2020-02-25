90143d7237f2f44adce7e0df35505b910e39d542
Mingqi Han <42781534+Stevenhanmq@users.noreply.github.com>
2020-02-25 18:58:08 -0500

    function skeletons

---
+++ server.js
@@ -0,0 +1,27 @@
+module.exports= {
+
+  signup: function(fb, db, email, password, name, nickname){
+    
+  },
+
+  login: function(fb, db, email, password, name, nickname){
+
+  },
+
+  logout: function(fb, db, email, password, name, nickname){
+
+  },
+
+  resetPassword: function(fb, email, password, name, nickname){
+
+  },
+
+  addTag: function(db, tag){
+
+  }
+
+  assignTag: function(){
+    
+  }
+
+}
