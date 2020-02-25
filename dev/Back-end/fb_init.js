d023fad30b1094770266c387cfa23b65576c8be3
WUu422 <wu1037@purdue.edu>
2020-02-24 20:01:05 -0500

    firebase init

---
+++ dev/Back-end/fb_init.js
@@ -0,0 +1,18 @@
+let _firebase = require('firebase');
+var config = {
+    apiKey: "AIzaSyB8fmbeFCz6MflVeRF-pU7N5WyPCDbQDQw",
+    authDomain: "in-request.firebaseapp.com",
+    databaseURL: "in-request.firebaseio.com"
+};
+
+function f2(){
+    _firebase.initializeApp(config);
+}
+
+async function fb_init(){
+    // firebase
+    await f2();
+    return _firebase;
+}
+
+module.exports = fb_init;
