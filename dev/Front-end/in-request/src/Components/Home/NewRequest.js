2fe5c7bc4824b672e4903a3004579077b5ffe4f9
ShiwenXu <vicky.xu071@gmail.com>
2020-02-25 20:39:55 -0500

    view with date picker

--- dev/Front-end/in-request/src/Components/Home/NewRequest.js
+++ dev/Front-end/in-request/src/Components/Home/NewRequest.js
@@ -5,6 +5,10 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import { withStyles } from "@material-ui/core/styles";
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const styles = theme => ({
   container: {
@@ -19,12 +23,54 @@ const styles = theme => ({
 });
 class NewRequest extends Component {
   constructor(props) {
-      super(props);
-      this.state={ date : new Date()};
+      super(props)
+      this.state = {
+        itemName: "",
+        description: "",
+        estimateVal: "",
+        returnDate: new Date(),
+      };
+      // This binding is necessary to make `this` work in the callback
+      this.handleNameChange = this.handleNameChange.bind(this);
+      this.handleDescription = this.handleDescription.bind(this);
+      this.handleValue = this.handleValue.bind(this);
+      this.handleDateChange = this.handleDateChange.bind(this);
+      this.onFormSubmit = this.onFormSubmit.bind(this);
   }
-  handleChange(e, date) {
-   this.setState({date: date})
+  handleNameChange = event => {
+    const name_val = event.target.value;
+    this.setState(state => ({
+      itemName: name_val
+    }));
   }
+
+  handleDescription = event => {
+    const description_val = event.target.value;
+    this.setState(state =>({
+      description: description_val
+    }));
+  }
+
+  handleValue = event => {
+    const estimate_val = event.target.value;
+    if (!Number(estimate_val)) {
+      alert("Numbers Only");
+      return;
+    }
+    this.setState(state => ({
+      estimateVal: estimate_val
+    }));
+  }
+  handleDateChange(date) {
+     this.setState({
+       returnDate: date
+     })
+  }
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.returnDate)
+  }
+  /*--- Still need a submit function---*/
   render() {
     const { classes } = this.props;
     return (
@@ -36,10 +82,12 @@ class NewRequest extends Component {
           <Grid item xs={12} >
             <TextField
               required
-              id="itemName"
+              id="item_Name"
               name="itemName"
               label="Item"
               fullWidth
+              value = {this.state.itemName}
+              onChange = {this.handleNameChange}
             / >
           </Grid>
           <Grid item xs={12}>
@@ -52,33 +100,36 @@ class NewRequest extends Component {
               rows={2}
               rowsMax={12}
               fullWidth
+              value = {this.state.description}
+              onChange = {this.handleDescription}
             />
           </Grid>
           <Grid item xs={12}>
             <TextField
               required
-              id="estimateVal"
+              id="estimate_Val"
               name="estimateVal"
               label="Estimate Value"
+              value = {this.state.estimateVal}
+              onChange = {this.handleValue}
               rows={1}
             />
           </Grid>
           <br />
-          <form className={classes.container} noValidate>
-            <TextField
-              id="returnTime"
-              label="Estimated Return Time"
-              type="returnTime"
-              defaultValue={this.state.date}
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </form>
         </Grid>
+        <br />
+        <form onSubmit={ this.onFormSubmit }>
+            <div className="form-group">
+              <DatePicker
+                  selected={ this.state.returnDate }
+                  onChange={ this.handleDateChange }
+                  name="startDate"
+                  dateFormat="MM/dd/yyyy"
+              />
+            </div>
+        </form>
       </Fragment>
-      );
+    );
   }
 }
 export default withStyles(styles, {withTheme: true})(NewRequest);
