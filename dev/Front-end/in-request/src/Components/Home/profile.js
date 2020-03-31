d8c0d99c107bb6dd3e3e9270a3383adc9aa4afe0
shuqi1222 <ma460@purdue.edu>
2020-03-31 16:35:41 -0400

    merge

--- dev/Front-end/in-request/src/Components/Home/profile.js
+++ dev/Front-end/in-request/src/Components/Home/profile.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -6,13 +6,6 @@ import IconButton from '@material-ui/core/IconButton';
 /* --- THIS FILE DISPLAY USER'S PERSONAL INFO --- */
 class Profile extends Component {
   render() {
-    return (
-      <div>
-        <h1>
-          THIS DISPLAYS EVERTHING ABOUT USER
-        </h1>
-      </div>
-    );
+    return <span>PROFILE PAGE</span>;
   }
 }
-export default Profile
